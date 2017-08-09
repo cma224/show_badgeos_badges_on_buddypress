ec1f9dc047ecef5e048b56cf0b502292f85aa6e3
Cristian Abello <cristian.abello@valpo.edu>
2017-08-09 20:51:21 +0000

    beta version for Valpo IN_Advance

--- loader.php
+++ loader.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Show BadgeOS Badges on bbPress
-Description: This plugin shows the badges earned by topic authors in BBPress.
+Description: This plugin shows the (configured) badges earned by posters in BBPress.
 Version: 1.0.0
 Author: Cristian Abello
 Author URI: mailto:cristian.abello@valpo.edu
@@ -23,20 +23,22 @@ class BadgeOS_bbPress_Extension{
     
         // If BadgeOS is unavailable, deactivate our plugin
 		add_action( 'admin_notices', array( $this, 'maybe_disable_plugin' ) );
-		add_action( 'init', array( $this, 'textdomain' ) );
-	
+
+        add_action('admin_init',array($this,'hideUserRoles'));
+        
+        // Display configured BadgeOS badges in bbPress
 		add_action( 'bbp_theme_after_user_profile', array( $this, 'arnedBadgesAfterReplyAuthorDetails' ) );
-		add_action( 'bbp_theme_after_reply_author_details', array( $this, 'earnedBadgesAfterReplyAuthorDetails' ) );
+		
+		add_action( 'bbp_theme_after_reply_author_details', array( $this, 'earnedBadges' ) );
 		
 		// Include our other plugin files
 		add_action( 'init', array( $this, 'includes' ) );
-       
-
+     
     }
 
+    public function earnedBadges($reply_id) {
         
-    public function earnedBadgesAfterReplyAuthorDetails($reply_id) {
-       // global $user_ID;
+        global $user_ID;
 
 		echo $args['before_widget'];
 
@@ -45,21 +47,66 @@ class BadgeOS_bbPress_Extension{
 		if ( !empty( $title ) ) { echo $args['before_title'] . $title . $args['after_title']; };
 
 		//user must be logged in to view earned badges and points
-		if ( is_user_logged_in() ) {
-$user_id = bbp_get_reply_author_id( $reply_id );
-			//display user's points if widget option is enabled
-			if(badgeos_has_user_earned_achievement( 223, $user_id)){
-				echo 'asdf';
-			}
+	
+        $user_id = bbp_get_reply_author_id( $reply_id );
 		
-           
-			}
-
-	}
-    	
-    	
-    public function textdomain() {
-		load_plugin_textdomain( 'bbp_private_replies', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	    // Admins and mentors
+	    if(badgeos_has_user_earned_achievement(2430, $user_id))
+			echo '<span class="admin-rank"><br>Admin</span>';
+		
+		if(badgeos_has_user_earned_achievement(2428, $user_id))
+			echo '<span class="mentor-rank"><br>Mentor</span>';
+		
+	    // Badges in progress	
+	    
+	    // Start badging as freshman/sophomore/junior/senior dependency
+		if(badgeos_has_user_earned_achievement(2243, $user_id) && !badgeos_has_user_earned_achievement(2420, $user_id))
+			echo '<span class="ft-ceo-rank"><br><br>FT CEO<br>[in Progress]<br><br></span>';
+		
+		if(badgeos_has_user_earned_achievement(2240, $user_id)
+	    && !badgeos_has_user_earned_achievement(2009, $user_id))
+			echo '<span class="ft-director-rank"><br>FT Director<br>[in Progress]<br><br></span>';
+		
+		if(badgeos_has_user_earned_achievement(2235, $user_id) && !badgeos_has_user_earned_achievement( 2012, $user_id))
+			echo '<span class="associate-rank"><br>FT Associate<br>[in Progress]<br><br></span>';
+		
+		if(badgeos_has_user_earned_achievement( 2231, $user_id) && ! badgeos_has_user_earned_achievement( 2010, $user_id))
+			echo '<span class="recruit-rank"><br>Recruit<br>[in Progress]<br><br></span>';
+		
+		if((badgeos_has_user_earned_achievement( 2008, $user_id)
+			|| badgeos_has_user_earned_achievement( 2009, $user_id) ) && ! badgeos_has_user_earned_achievement( 2013, $user_id))
+			echo '<span class="ceo-rank"><br>CEO<br>[in Progress]<br><br></span>';
+		
+		if((badgeos_has_user_earned_achievement( 2010, $user_id)
+		 ) && ! badgeos_has_user_earned_achievement( 2011, $user_id))
+			echo '<span class="associate-rank"><br>Associate<br>[in Progress]<br><br></span>';
+		
+		if(((badgeos_has_user_earned_achievement( 2011, $user_id)
+		 )||badgeos_has_user_earned_achievement( 2012, $user_id) ) && ! badgeos_has_user_earned_achievement( 2008, $user_id))
+			echo '<span class="associate-rank"><br>Director<br>[in Progress]<br><br></span>';
+		
+	    // Earned badges
+		if(badgeos_has_user_earned_achievement( 2013, $user_id))
+			echo '<span class="ceo-rank">CEO<br><br></span>';
+		
+		if(badgeos_has_user_earned_achievement( 2420, $user_id))
+			echo '<span class="ft-ceo-rank">FT CEO<br><br></span>';
+		
+		if(badgeos_has_user_earned_achievement( 2008, $user_id))
+			echo '<span class="director-rank">Director<br><br></span>';
+		
+		if(badgeos_has_user_earned_achievement( 2009, $user_id))
+			echo '<span class="ft-director-rank">FT Director<br><br></span>';
+		
+		if(badgeos_has_user_earned_achievement( 2011, $user_id))
+			echo '<span class="associate-rank">Associate<br><br></span>';
+		
+		if(badgeos_has_user_earned_achievement( 2012, $user_id))
+			echo '<span class="ft-associate-rank">FT Associate<br><br></span>';
+		
+		if(badgeos_has_user_earned_achievement( 2010, $user_id))
+			echo '<span class="recruit-rank">Recruit<br><br></span>';
+	
 	}
     
     public function includes() {
@@ -81,12 +128,11 @@ $user_id = bbp_get_reply_author_id( $reply_id );
     public static function meets_requirements() {
         
         // class_exists checks that BadgeOS, BuddyPress, and bbPress are all ACTIVE
-        if(class_exists('BadgeOS') && class_exists('BuddyPress') && class_exists('bbPress'))
+        if(class_exists('BadgeOS') && class_exists('BuddyPress') && class_exists('bbPress') /*&& wp_get_current_user=='inadvance'*/)
             return true;
         else
             return false;
-        
-        
+            
     }
     
     public function maybe_disable_plugin() {
@@ -106,9 +152,16 @@ $user_id = bbp_get_reply_author_id( $reply_id );
     				echo '<p>' . sprintf( __( 'This plugin requires BuddyPress and has been <a href="%s">deactivated</a>. Please install and activate BuddyPress and then reactivate this plugin.', 'badgeos-addon' ), admin_url( 'plugins.php' ) ) . '</p>';
     				echo '</div>';
     			}
+    			
     			if(!class_exists('bbPress')){
     				echo '<div id="message" class="error">';
     				echo '<p>' . sprintf( __( 'This plugin requires bbPress and has been <a href="%s">deactivated</a>. Please install and activate bbPress and then reactivate this plugin.', 'badgeos-addon' ), admin_url( 'plugins.php' ) ) . '</p>';
+    				echo '</div>';
+    			}
+    			
+    			if(wp_get_current_user!='inadvance' || wp_get_current_user!='cabello'){
+    			    echo '<div id="message" class="error">';
+    				echo '<p>' . sprintf( __( 'This plugin is for the sole use of the IN_Advance program and has been <a href="%s">deactivated</a>. If your website is running BadgeOS and would like a tailored version of this plugin, please contact <a href="mailto:cristian.abello@valpo.edu">Cristian Abello</a>.', 'badgeos-addon' ), admin_url( 'plugins.php' ) ) . '</p>';
     				echo '</div>';
     			}
     
